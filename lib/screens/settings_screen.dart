176794dcc764034dc05733b9e24a0a2905b0ee33
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2023-02-21 16:33:12 -0700

    radio button & switch

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:preferences_app/widgets/widgets.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   static const String routerName = 'Settings';
   const SettingsScreen({super.key});
 
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool isDarkMode = false;
+  int gender = 1;
+  String name = 'Roberto';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,26 +31,39 @@ class SettingsScreen extends StatelessWidget {
                   style: TextStyle(fontSize: 45, fontWeight: FontWeight.w300),
                 ),
                 const Divider(),
-                SwitchListTile(
+                SwitchListTile.adaptive(
                     title: const Text('Darkmode'),
-                    value: true,
-                    onChanged: (value) {}),
+                    value: isDarkMode,
+                    onChanged: (value) {
+                      isDarkMode = value;
+                      setState(() {});
+                    }),
                 const Divider(),
                 RadioListTile<int>(
                     title: const Text('Masculino'),
                     value: 1,
-                    groupValue: 1,
-                    onChanged: (value) {}),
+                    groupValue: gender,
+                    onChanged: (value) {
+                      gender = value ?? 1;
+                      setState(() {});
+                    }),
                 const Divider(),
                 RadioListTile<int>(
                     title: const Text('Femenino'),
                     value: 2,
-                    groupValue: 1,
-                    onChanged: (value) {}),
+                    groupValue: gender,
+                    onChanged: (value) {
+                      gender = value ?? 2;
+                      setState(() {});
+                    }),
                 const Divider(),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: TextFormField(
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                     initialValue: 'Roberto',
                     decoration: const InputDecoration(
                         labelText: 'Nombre', helperText: 'Nombre del usuario'),
